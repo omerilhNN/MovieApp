f35839dfb44b288ac148d9ab4a200c583647b920
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2023-08-07 15:37:37 +0300

    MovieList

--- app/src/main/java/com/omrilhn/movieapp/MainActivity.kt
+++ app/src/main/java/com/omrilhn/movieapp/MainActivity.kt
@@ -1,25 +1,33 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
+@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
 
 package com.omrilhn.movieapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -28,8 +36,10 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.omrilhn.movieapp.ui.theme.MovieAppTheme
@@ -44,6 +54,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
     MovieAppTheme {
@@ -56,7 +67,7 @@ fun MyApp(content: @Composable () -> Unit) {
                         .fillMaxWidth())
             }
         }) {paddingValues ->
-                Text(paddingValues.toString())
+           Log.d("Padding","$paddingValues")
         }
     }
 }
@@ -81,10 +92,26 @@ fun MovieRow(movie:String){
     Card(modifier = Modifier
         .padding(4.dp)
         .fillMaxWidth()
-        .height(130.dp),
+        .height(130.dp)
+        .clickable {
+                   
+        },
         shape = RoundedCornerShape(corner = CornerSize(16.dp)),
         elevation = CardDefaults.cardElevation(6.dp)){
-            Text(text = movie)
+            Row(verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start){
+                Surface(modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),shape = RectangleShape,
+                    shadowElevation = 5.dp){
+
+                    Icon(imageVector = Icons.Default.AccountBox
+                        , contentDescription ="Movie Image" )
+
+                }
+                Text(text = movie)
+
+            }
     }
 }
 
